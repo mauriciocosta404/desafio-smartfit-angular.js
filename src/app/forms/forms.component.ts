29f47cfc60707a0306c9e069727cd9be7ef932e6
mauriciocosta404 <mc611641@gmail.com>
2024-10-02 09:19:55 +0100

    feat: adicionando a feat de cleanup

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
 
 export class FormsComponent implements OnInit{
   results = [];
+  formGroup!: FormGroup;
 
-  constructor(){}
+  constructor(private formBuilder: FormBuilder){}
 
   ngOnInit(): void {
-    
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    });
+  }
+
+  onSubmit(): void{
+    console.log(this.formGroup.value);
+  }
+
+  onCleanUp(): void{
+    this.formGroup.reset();
   }
 }
