28e3ed0658625b08ae28eeb827e0f14705acd5dd
mauriciocosta404 <mc611641@gmail.com>
2024-10-04 10:45:58 +0100

    feat: adicionando a legenda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { CardsListComponent } from "./components/cards-list/cards-list.component
 import { CommonModule } from '@angular/common';
 import { Location } from './types/units-response.interface';
 import { GetUnitsService } from './services/get-units.service';
+import { LegendComponent } from "./components/legend/legend.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent, CardsListComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, FormsComponent, CardsListComponent, CommonModule, LegendComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
